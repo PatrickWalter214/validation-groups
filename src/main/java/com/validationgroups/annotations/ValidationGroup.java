7f66ea1688d3def9f341336efa798116e62bc944
PatrickWalter214 <patrick.walter214@gmail.com>
2022-04-01 21:00:01 +0200

    Add javadoc

--- src/main/java/com/validationgroups/annotations/ValidationGroup.java
+++ src/main/java/com/validationgroups/annotations/ValidationGroup.java
@@ -2,6 +2,9 @@ package com.validationgroups.annotations;
 
 import java.lang.annotation.*;
 
+/**
+ * Used to mark a field as part of a specific validation group.
+ */
 @Target( { ElementType.FIELD })
 @Retention(RetentionPolicy.RUNTIME)
 @Repeatable(ValidationGroup.List.class)
